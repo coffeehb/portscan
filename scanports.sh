9eabcf28b0cc64f4fbe8bcf3f6fc42387211ea26
Komi <coffeehb@users.noreply.github.com>
2018-11-29 00:20:04 +0800

    bash版的端口爆破
    使用例子：爆破65.61.137.117的1-10000端口
    sh scanports.sh 65.61.137.117 1 10000
    默认的并发数是number_tasks=50，可以自己修改

---
+++ scanports.sh
@@ -0,0 +1,54 @@
+
+#! /bin/bash
+source /etc/profile;
+# https://www.jianshu.com/p/2d60e6513fdd
+# -----------------------------
+#
+#➜  portscan> sh bash_scanports.sh 23.232.72.205 1 10000
+#Connection to 23.236.75.205 port 22 [tcp/ssh] succeeded!
+#Connection to 23.236.75.205 port 80 [tcp/http] succeeded!
+#Connection to 23.236.75.205 port 8443 [tcp/pcsync-https] succeeded!
+#done!!!!!!!!!!
+
+tempfifo=$$.fifo
+target_ip=$1
+start_port=$2
+end_port=$3
+number_tasks=50
+
+if [ $# -eq 3 ] 
+then
+    if [ "$start_port" \> "$end_port" ]
+    then
+        echo "Error! $start_port is greater than $end_port"
+        exit 1;
+    fi
+else
+    echo "Error! Not enough params."
+    echo "Sample: sh bash_scanports.sh 23.232.72.205 1 10000"
+    exit 2;
+fi
+
+trap "exec 1000>&-;exec 1000<&-;exit 0" 2 # Ctrl + C
+mkfifo $tempfifo
+exec 1000<>$tempfifo
+rm -rf $tempfifo
+
+for ((i=1; i<$number_tasks; i++))
+do
+    echo >&1000
+done
+
+while [ $start_port != $end_port ]
+do
+    read -u1000
+    {
+        nc -z -w 1 $target_ip $start_port | grep succeeded
+        echo >&1000
+    } &
+
+    let start_port=start_port+1
+done
+
+wait
+echo "done!!!!!!!!!!"
